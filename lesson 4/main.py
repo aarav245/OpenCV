147c1df52de1a494d726f94c58dd3ed56c004060
aarav245 <aaravmisra24@gmail.com>
2025-11-16 19:26:33 -0800

    main.py

--- lesson 4/main.py
+++ lesson 4/main.py
@@ -12,8 +12,8 @@ for i in os.listdir('.'):
     width,height = openimg.size
     averagew = averagew+width
     averageh = averageh+height
-averagew = averagew/imagenumber
-averageh = averageh/imagenumber
+averagew = int(averagew/imagenumber)
+averageh = int(averageh/imagenumber)
 print(averagew,averageh)
 for i in os.listdir('.'):
     #checking file extensions
@@ -24,6 +24,26 @@ for i in os.listdir('.'):
         resizeimg = openimg.resize((averagew,averageh),Image.LANCZOS)
         resizeimg.save(i,"JPEG",quality = 95)
         print(openimg.filename.split('\\')[-1],"is resized")
+#function for generating video
+def video():
+    filen = "newvideo.avi"
+    os.chdir(r"C:\Users\Aarav\OneDrive\Desktop\openCV\lesson 4\images")
+    filelist = []
+    for i in os.listdir("."):
+        if i.endswith(".jpg") or i.endswith(".jpeg") or i.endswith(".png"):
+            filelist.append(i)
+    print(filelist)
+    img = cv2.imread(os.path.join(".",filelist[0]))
+    height,width,layers = img.shape
+    vid = cv2.VideoWriter(filen,0,1,(width,height))
+    for u in filelist:
+        vid.write(cv2.imread(os.path.join(".",u)))
+    cv2.destroyAllWindows()
+    vid.release()
+    print("video is saved")
+
+video()
+
 
 
 
