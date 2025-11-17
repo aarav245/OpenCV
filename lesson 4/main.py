544db3ef1895b6632e85d328e9a4462b09542609
aarav245 <aaravmisra24@gmail.com>
2025-11-16 19:08:58 -0800

    main.py

--- lesson 4/main.py
+++ lesson 4/main.py
@@ -9,7 +9,21 @@ averagew = 0
 imagenumber = len(os.listdir("."))
 for i in os.listdir('.'):
     openimg = Image.open(os.path.join(path,i))
-    width,height = i.size
+    width,height = openimg.size
     averagew = averagew+width
     averageh = averageh+height
+averagew = averagew/imagenumber
+averageh = averageh/imagenumber
 print(averagew,averageh)
+for i in os.listdir('.'):
+    #checking file extensions
+    if i.endswith(".jpg") or i.endswith(".jpeg") or i.endswith(".png"):
+        openimg = Image.open(os.path.join(path,i))
+        width,height = openimg.size
+        print(width,height)
+        resizeimg = openimg.resize((averagew,averageh),Image.LANCZOS)
+        resizeimg.save(i,"JPEG",quality = 95)
+        print(openimg.filename.split('\\')[-1],"is resized")
+
+
+
