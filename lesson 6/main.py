121e86b19a6cc60f494931071e872dcd4052eaec
aarav245 <aaravmisra24@gmail.com>
2025-11-30 18:43:09 -0800

    main.py

--- lesson 6/main.py
+++ lesson 6/main.py
@@ -6,7 +6,7 @@ params = cv2.SimpleBlobDetector_Params()
 params.filterByArea = True
 params.minArea = 90
 params.filterByCircularity = True
-params.minCircularity = 0.7
+params.minCircularity = 0.9
 params.filterByConvexity = True
 params.minConvexity = 0.2
 params.filterByInertia = True
@@ -15,5 +15,8 @@ detector = cv2.SimpleBlobDetector_create(params)
 blobsdetected = detector.detect(image)
 blank = np.zeros((1,1))
 blobs = cv2.drawKeypoints(image,blobsdetected,blank,(0,0,255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+#counting blobs
+numberblobs = "Number of Circular blobs: "+str(len(blobsdetected))
+cv2.putText(blobs,numberblobs,(0,540),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,0),2)
 cv2.imshow("screen",blobs)
 cv2.waitKey(0)
